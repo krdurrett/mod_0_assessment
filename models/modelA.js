9bb61395cb5374d0cc2fc363a19e25477d95356c
Kayla Durrett <kayla.durrett@gmail.com>
2021-09-03 09:24:27 -0600

    Add object in models

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+var dinosaur = {
+  name: "Bob",
+  period: "Cretaceous",
+  species: "Tyrannosaurus",
+  age: 66000000,
+  heightInFeet: 20,
+  scary: true,
+  aliveToday: false,
+  foods: ["hadrosaurs", "sauropods", "ankylosaurs"],
+  features: ["Big head", "Tiny arms", "angry"],
+}
