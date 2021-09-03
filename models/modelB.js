b5afe584902525ae94a57b409149294f61cc5b26
Kayla Durrett <kayla.durrett@gmail.com>
2021-09-03 09:31:15 -0600

    Add developer object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  name: "Kayla",
+  mindset: "growth",
+  age: 38,
+  timeInIndustryInYears: 0.1,
+  asksForHelp: true,
+  canProgramOnOwn: false,
+  resourcesForCoding: ["Google", "cohort mates", "Stack Oveflow"],
+  activitiesForSucces: ["Coding Practice", "Studying Notes", "More Practice"],
+}
